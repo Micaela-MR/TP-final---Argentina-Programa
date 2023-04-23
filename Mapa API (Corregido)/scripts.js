63e3ab016d72b715affad374b38a62905f98a9e5
Christian-I-M <130510750+Christian-I-M@users.noreply.github.com>
2023-04-23 12:44:05 -0300

    Add files via upload

---
+++ Mapa API (Corregido)/scripts.js
@@ -0,0 +1,32 @@
+let mapOptions = {
+    center:[-34.6879893, -58.5850811],
+    zoom:10
+}
+
+let map = new L.map('map' , mapOptions);
+
+let layer = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+map.addLayer(layer);
+
+let customIcon = {
+    iconUrl:"https://icons.iconarchive.com/icons/icons-land/vista-map-markers/128/Map-Marker-Push-Pin-1-Pink-icon.png",
+    iconSize:[40,40]
+}
+
+let myIcon = L.icon(customIcon);
+//let myIcon = L.divIcon();
+
+let iconOptions = {
+    title:"company name",
+    draggable:true,
+    icon:myIcon
+}
+
+let marker = new L.Marker([-34.6879893, -58.5850811] , iconOptions);
+marker.addTo(map);
+marker.bindPopup("<span style='font-size: 18px;'>Dog King: Pet Shop<br>Cerviño 4719, Isidro Casanova</span>").openPopup();
+
+//http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+
+
+
